b276217f5da9dea1812569140e78fec9e8dd6e39
hyperpro <zhangbjb@126.com>
2012-10-07 11:36:24 +0800

    modify the index

--- webserver/main.py
+++ webserver/main.py
@@ -35,6 +35,18 @@ for i in range(1, 8):
 
 default_video = model.Video('video1', 'test1', 'intro of video1', 'upload_time1', 1)
 
+### Main Applicaiton 
+app = web.application(urls, globals())
+
+### User Sessions
+web.config.debug = False
+session = web.session.Session(app,web.session.DiskStore('sessions'),initializer={'login':"", 'username':'guest'})
+web.config.session_parameters['ignore_expiry'] = False
+web.config.session_parameters['timeout'] = 70
+web.config.session_parameters['exprired_message'] = 'Please login again'
+
+
+
 ### FileServer
 fs = fileServerApi.FileServer('http://localhost:17007')
 
@@ -48,6 +60,16 @@ class PageInfo:
 class Index:
 
 	def GET(self):
+		cookie = web.cookies();
+		if cookie:
+			if session.username == cookie.value:
+				current_user = Getuser(cookie.value)
+				return render.index(current_user, page_info)
+			else:
+				raise web.seeother('/login')
+		else:
+			raise web.seeother('/login')
+			 
 		page_info = PageInfo('Index')
 		return render.index(default_user, page_info)
 		#return elements.video_prev(default_video)
@@ -117,9 +139,6 @@ class Edit:
 		page_info = PageInfo('Edit')
 		return render.edit(default_video, default_user, page_info)
 
-
-###
-app = web.application(urls, globals())
 
 if __name__ == '__main__':
 	app.run()
