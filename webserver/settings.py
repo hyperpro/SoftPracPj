366bdff4a5ce5c8f50a07d78dd014a54412ce80f
hyperpro <zhangbjb@126.com>
2012-10-08 15:01:55 +0800

    database pwd

--- webserver/settings.py
+++ webserver/settings.py
@@ -4,5 +4,5 @@ import web,os
 fileServerUname = 'zhangxu'
 fileServerPword = 'adc8403dd40a5bf151be81c346cd0f4476459733'
 
-db = web.database(dbn='mysql', db = 'videoproject', user = 'root', passwd = '901018')
+db = web.database(dbn='mysql', db = 'videoproject', user = 'root', passwd = '')
 
