bcae5fc2e615743ce11a3c5ef011ac625cfe5cb6
Hanxing Yang <nighca@live.cn>
2012-10-09 15:39:56 +0800

    do with edit page

--- webserver/static/js/edit.js
+++ webserver/static/js/edit.js
@@ -1,12 +1,13 @@
 $(function(){
-	$('#login_form input').each(function(){
+	$('#edit_form input').each(function(){
 		var tip = $(this).next('.form_tip');
 		if($(this).val()==''){
 			tip.addClass('error').text("Can't be blank");
 		}
 	});
 
-	$('#login_form input').on('blur', function(){
+	$('#edit_form input').on('blur', function(){
+		console.log("!!!");
 		var tip = $(this).next('.form_tip');
 		if($(this).val()==''){
 			tip.addClass('error').text("Can't be blank").fadeIn('slow').delay(2000).fadeOut('slow');
@@ -16,8 +17,8 @@ $(function(){
 	});
 	
 	$('#submit').on('click', function(){
-		if($('#login_form .error').length>0){
-			$('#login_form .error').fadeIn('slow').delay(2000).fadeOut('slow');
+		if($('#edit_form .error').length>0){
+			$('#edit_form .error').fadeIn('slow').delay(2000).fadeOut('slow');
 			return false;
 		}else{
 			return true;
