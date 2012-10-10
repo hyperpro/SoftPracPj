591bbae79be7e9186e88173efd853a6b7821d5d7
Hanxing Yang <nighca@live.cn>
2012-10-10 14:16:26 +0800

    sv

--- webserver/static/js/index.js
+++ webserver/static/js/index.js
@@ -1,12 +1,12 @@
 $(function(){
 	$('.video_block').on('click', function(){
-		$('#block2').removeClass('wide_block');
+		$('#block2').removeClass('wide_block').addClass('normal_block');
 		$('#block3').removeClass('hide_block');
 		$('#expand').show();
 	});
 
 	$('#expand').on('click', function(){
-		$('#block2').addClass('wide_block');
+		$('#block2').removeClass('normal_block').addClass('wide_block');
 		$('#block3').addClass('hide_block');
 		$('#expand').hide();
 	});
